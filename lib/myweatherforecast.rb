2b9b5490438626109baab89dca8a594a1487b6d3
Your Name <you@example.com>
2015-10-23 21:39:18 +0100

    Refactoring; The class names are now closer to the data they represent. Improved the results returned from queries including returning the weather forecast for the morning, afternoon, evening, or night.

--- lib/myweatherforecast.rb
+++ lib/myweatherforecast.rb
@@ -14,6 +14,8 @@ require 'open-uri'
 # SI explained: https://en.wikipedia.org/wiki/SI_derived_unit
 
 class MyWeatherForecast
+  
+  attr_reader :coordinates
 
   def initialize(*location, api_key: nil, units: :auto, timeout: 3)
     
@@ -36,7 +38,7 @@ class MyWeatherForecast
                                                                    results.any?
       results[0].coordinates
                               
-    end
+    end        
 
     ForecastIO.api_key = api_key
 
@@ -46,27 +48,54 @@ class MyWeatherForecast
     autounits = @forecast['flags']['units']
     
     @tlabel = autounits == 'us' ? '°F' : '°C'      
+    @coordinates = [lat, lon]
     
   end
 
-  class Day
+  class Hourly
+    
+    attr_reader :today
 
-    def initialize(forecast, tlabel, i=nil)
+    def initialize(forecast, tlabel, i=0)
       
-      @forecast = forecast
-      @x = i ? forecast['hourly']['data'][i] : forecast.currently
-      @tlabel = tlabel
+      @forecast, @tlabel, @i = forecast, tlabel, i
+      
+      @x, @hourly_data = if i > 0 then
+        [forecast['hourly']['data'][i], forecast['hourly']['data'][i..-1]]
+      else
+        [forecast.currently, forecast['hourly']['data']]
+      end
 
+      found = @forecast['daily']['data'].detect do |day|
+        self.time.to_date == Time.at(day.time).to_date
+      end
+      
+      @day = Daily.new found, @tlabel
+      
     end    
     
     def at(raw_hour)
       
       hour = Time.parse(raw_hour).hour
       i = 0
-      i += 1 until Time.at(@forecast['hourly']['data'][i]['time']).hour \
-                                                                  ==  hour.to_i
-      Day.new(@forecast, @tlabel, i)
+
+      return if Time.at(@hourly_data[i]['time']).hour > hour
+      
+      i += 1 until Time.at(@hourly_data[i]['time']).hour ==  hour
+
+      Hourly.new(@forecast, @tlabel, i+@i)
+
     end
+    
+    def afternoon()   period(12, 17)                end    
+    def early_hours() period(0, 5)                  end
+    def evening()     period(17, night_time.hour+1) end
+    def morning()     period(6, 12)                 end    
+    def night()       period(night_time.hour, 23)   end
+    
+    def night_time()
+      Time.at(@day.sunsetTime)
+    end        
     
     def humidity()
       @x.humidity
@@ -75,18 +104,37 @@ class MyWeatherForecast
     def icon()
       @x.icon
     end
+    
+    def noon()
+      at_hour 12
+    end
+    
+    alias midday noon
 
     def to_s
-      "%d%s, %s" % [@x.temperature.round, @tlabel, @x.summary]
+      "%s: %d%s, %s" % [self.time.strftime("%-I%P"), @x.temperature.round, \
+                                                          @tlabel, @x.summary]
     end
 
     def summary()
       @x.summary
     end
     
+    def sunrise()
+      @day.sunrise
+    end
+        
+    def sunset()
+      @day.sunset
+    end    
+    
+    alias night_time sunset    
+    
     def temperature
       "%s°" % @x.temperature.round
     end
+    
+    alias temp temperature
     
     def time
       Time.at @x.time
@@ -100,47 +148,71 @@ class MyWeatherForecast
       @x.windSpeed.round
     end
     
+    private
+    
+    def at_hour(n)
+
+      i = 0
+
+      return if Time.at(@hourly_data[i]['time']).hour > n 
+      len = @hourly_data.length
+      i += 1 until Time.at(@hourly_data[i]['time']).hour ==  n or i >= len - 1
+
+      Hourly.new(@forecast, @tlabel, i+@i)
+    end
+    
+    def period(hr1, hr2)
+      
+      current_hour = Time.at(@hourly_data[0]['time']).hour      
+      
+      return if current_hour >= hr2
+      
+      hr1 = current_hour if current_hour > hr1
+      hr2 = @hourly_data.length - 2 if hr2 + 1 > @hourly_data.length - 1
+      (hr1..hr2).map {|n| at_hour n}
+      
+    end
+    
   end
   
-  class DaysAhead < Day
-    
-    def initialize(forecast, tlabel, index: 0)
+                                 
+  class Daily < Hourly
+                                 
+    def initialize(forecast, tlabel, d=0)
       
       @forecast = forecast
-      @x = forecast['hourly']['data'][index]      
+
+      @x = forecast['daily']['data'][d]
       @tlabel = tlabel
-      @i = index
+
+      found = forecast['hourly']['data'].detect do |hour|
+        Time.at(@x.time).to_date == Time.at(hour.time).to_date
+      end
       
+      @i = forecast['hourly']['data'].index found      
+      
+      return if @i.nil?
+
+      @hourly_data = forecast['hourly']['data'][@i..-1]
+      @day = self
+                                      
     end
     
-    def at(raw_hour)
-      
-      hour = Time.parse(raw_hour).hour
-
-      i = @i - 12
-      i += 1 until Time.at(@forecast['hourly']['data'][i]['time']).hour \
-                                                                  ==  hour.to_i
-      DaysAhead.new(@forecast, @tlabel, index: i)
-    end    
-
-  end
-                                 
-  class OutlookDay < Day
-                                 
-    def initialize(day, tlabel)
-      @x = day
-      @tlabel = tlabel
-    end
-    
-    def at(*a)
-      "%s outlook: %s - %s, %s" % [Date::DAYNAMES[self.time.wday], \
-                                                  tempmin, tempmax, @x.summary]
-    end
-
     def to_s
-      "%s: %s - %s, %s" % [Date::ABBR_DAYNAMES[self.time.wday], \
-                                                  tempmin, tempmax, @x.summary]
+      
+      label = self.time.to_date == Time.now.to_date ? 'Today' : \
+                                            Date::ABBR_DAYNAMES[self.time.wday]
+      
+      "%s: ▽%s ▲%s, %s" % [label, tempmin, tempmax, @x.summary]
     end
+    
+    def sunrise()
+      Time.at @x.sunriseTime
+    end
+    
+    def sunset()
+      Time.at @x.sunsetTime
+    end    
     
     def temperature()
     end
@@ -155,7 +227,7 @@ class MyWeatherForecast
     
 
   end
-
+  
   
   # e.g.
   # require 'myweatherforecast'
@@ -168,25 +240,41 @@ class MyWeatherForecast
   # Sun: 5° - 12°, Mostly cloudy throughout the day.
 
   def days()
-    @forecast['daily']['data'].map {|day| OutlookDay.new(day, @tlabel) }
+    (@forecast['daily']['data'].length).times.map {|n| Daily.new(@forecast, @tlabel, n) }
+  end
+
+  def hours()
+    
+    len = @forecast['hourly']['data'].length
+    len.times.map {|i| Hourly.new @forecast, @tlabel, i}
+
   end
   
   def today()
-    Day.new(@forecast, @tlabel)
+    Daily.new(@forecast, @tlabel)
   end
   
+  def tonight()
+    Daily.new(@forecast, @tlabel).night
+  end
+        
+  alias currently today
   alias now today
-
+  alias this today
+  
   def tomorrow()
     
     # select tomorrow at midday
     i = 0
     
-    i += 7 if Time.at(@forecast['hourly']['data'][i]['time']).hour >= 6
+    if Time.at(@forecast['hourly']['data'][i]['time']).hour == 0 then
+      i += 24 
+    else
+      i += 1 until Time.at(@forecast['hourly']['data'][i]['time']).hour ==  0
+    end
     
-    i += 1 until Time.at(@forecast['hourly']['data'][i]['time']).hour ==  12
-    DaysAhead.new(@forecast, @tlabel, index: i)
-  end
+    Hourly.new(@forecast, @tlabel, i)
+  end 
   
   def monday()    day :monday    end
   def tuesday()   day :tuesday   end
@@ -209,16 +297,11 @@ class MyWeatherForecast
   def day(name)
     
     name = (name.to_s + '?').to_sym
-    len = @forecast['hourly']['data'].length
-    i = 0
-    i += 1 until Time.at(@forecast['hourly']['data'][i]['time']).\
-                                              method(name).call or i >= len - 1
-    return DaysAhead.new(@forecast, @tlabel, index: i) unless i == len - 1
                                  
     d = 0 
     d += 1 until Time.at(@forecast['daily']['data'][d].time).method(name).call
     Time.at(@forecast['daily']['data'][d].time)
-    OutlookDay.new(@forecast['daily']['data'][d], @tlabel)    
+    Daily.new(@forecast, @tlabel, d)
   end
 
 end
--- myweatherforecast.gemspec
+++ myweatherforecast.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'myweatherforecast'
-  s.version = '0.4.0'
+  s.version = '0.5.0'
   s.summary = 'This gem is a wrapper of the forecast_io gem.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/myweatherforecast.rb']
